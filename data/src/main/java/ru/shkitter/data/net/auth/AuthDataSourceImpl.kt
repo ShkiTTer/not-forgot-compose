4d49471574f6c72d3bb7bc40feba1969e68ded21
Maxim Sachuk <msachuk@kode-t.ru>
2021-08-10 14:43:35 +0700

    Add save session

--- data/src/main/java/ru/shkitter/data/net/auth/AuthDataSourceImpl.kt
+++ data/src/main/java/ru/shkitter/data/net/auth/AuthDataSourceImpl.kt
@@ -1,17 +1,30 @@
 package ru.shkitter.data.net.auth
 
+import ru.shkitter.data.db.session.SessionDao
+import ru.shkitter.data.db.session.model.SessionEntity
 import ru.shkitter.data.net.auth.model.LoginBody
 import ru.shkitter.data.net.auth.model.RegistrationBody
 import ru.shkitter.domain.auth.AuthDataSource
 import ru.shkitter.domain.auth.model.ApiToken
 
-class AuthDataSourceImpl(private val authApi: AuthApi) : AuthDataSource {
+class AuthDataSourceImpl(
+    private val authApi: AuthApi,
+    private val sessionDao: SessionDao
+) : AuthDataSource {
 
     override suspend fun login(email: String, password: String): ApiToken {
-        return authApi.login(LoginBody(email, password)).toDomain()
+        return authApi.login(LoginBody(email, password)).toDomain().also {
+            sessionDao.saveSession(
+                SessionEntity(email, it.apiToken)
+            )
+        }
     }
 
     override suspend fun registration(email: String, password: String, name: String): ApiToken {
-        return authApi.registration(RegistrationBody(email, name, password)).toDomain()
+        return authApi.registration(RegistrationBody(email, name, password)).toDomain().also {
+            sessionDao.saveSession(
+                SessionEntity(email, it.apiToken)
+            )
+        }
     }
 }
